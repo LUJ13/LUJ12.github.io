013389b4e0a7e458b53c0e1794fc77bd17bfd07a
LUJ13 <ljq374075473@gmail.com>
2023-03-15 09:30:33 +1300

    niubi

---
+++ assets/js/home.js
@@ -0,0 +1,211 @@
+//link size options to different prices for card 1
+function productPrice1() {
+    var Size = document.getElementById("size1").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price1").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price1").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price1").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price1").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price1").innerHTML = "$" + price
+     }
+    
+  }
+  //link size options to different prices for card 2
+function productPrice2() {
+    var Size = document.getElementById("size2").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price2").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price2").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price2").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price2").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price2").innerHTML = "$" + price
+     }
+    
+  }
+    //link size options to different prices for card 3
+function productPrice3() {
+    var Size = document.getElementById("size3").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price3").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price3").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price3").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price3").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price3").innerHTML = "$" + price
+     }
+    
+  }
+      //link size options to different prices for card 4
+function productPrice4() {
+    var Size = document.getElementById("size4").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price4").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price4").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price4").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price4").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price4").innerHTML = "$" + price
+     }
+    
+  }
+      //link size options to different prices for card 5
+function productPrice5() {
+    var Size = document.getElementById("size5").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price5").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price5").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price5").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price5").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price5").innerHTML = "$" + price
+     }
+    
+  }
+      //link size options to different prices for card 6
+function productPrice6() {
+    var Size = document.getElementById("size6").value;
+    var price
+    if (Size == "Small")
+     {
+        price = 12
+        document.getElementById("product-price6").innerHTML = "$" + price
+
+     }
+     else if (Size == "Medium")  
+     {
+        price = 15
+        document.getElementById("product-price6").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Large")  
+     {
+        price = 18
+        document.getElementById("product-price6").innerHTML = "$" + price
+
+     } 
+     else if (Size == "ExtraLarge")  
+     {
+        price = 22
+        document.getElementById("product-price6").innerHTML = "$" + price
+
+     } 
+     else if (Size == "Standard")
+     {
+        price = 18
+        document.getElementById("product-price6").innerHTML = "$" + price
+     }
+    
+  }
+  /*increasing item number of cart by clicking add to cart icon*/
